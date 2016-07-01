9162ec0299e4eecf03ef2059562a07ae33460f8a
joegotflow83 <joe@absolutod.com>
2016-06-30 20:40:34 -0400

    Working on native Map functions

--- maps/maps.exs
+++ maps/maps.exs
@@ -16,4 +16,22 @@ defmodule MapTest do
     assert sample[:foo] == 'bar'
     assert sample[:non_existant] == nil
   end
+
+  test "." do
+    assert sample.foo == 'bar'
+    assert_raise KeyError, fn ->
+      sample.non_existant
+    end
+  end
+
+  test "Map.fetch" do
+    {:ok, val} = Map.fetch(sample, :foo)
+    assert val == 'bar'
+    :error = Map.fetch(sample, :non_existant)
+  end
+
+  test "Map.put" do
+    assert Map.put(sample, :foo, 'bob') == %{foo: 'bob', baz: 'quz'}
+    assert Map.put(sample, :far, 'bar') == %{foo: 'bar', baz: 'quz', far: 'bar'}
+  end
 end
